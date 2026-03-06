55b01311071dea3ab6b0901a9aaab7576627f8d6
david-rai <davidkhaling123@gmail.com>
2026-03-06 22:09:01 +0545

    PRessable images

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { images } from "../../constants/images.js";
+import { useState, useEffect } from "react";
+import { Pressable } from "react-native";
+import { Modal } from "react-native";
+import { View, Image } from "react-native";
+import { ScrollView } from "react-native";
 
-export default function index() {
+const Home = () => {
+  const [selectedImage, setSelectedImage] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShow = (img) => {
+    console.log("image pressed");
+    setSelectedImage(img);
+    setShowModal(true);
+  };
   return (
-    <View className="">
-      <Text>index</Text>
-    </View>
-  )
-}
+    <ScrollView contentContainerClassName="p-4 flex-row flex-wrap gap-4 items-center justify-center">
+      {images.map((img, index) => (
+        <Pressable
+          key={index}
+          onPress={() => handleShow(img)}
+          className="h-[220] w-[45%] rounded-lg"
+        >
+          <Image
+            source={img}
+            className="flex-1"
+            resizeMode="cover"
+          />
+        </Pressable>
+      ))}
+      <Modal visible={showModal}>
+        <Image source={selectedImage} />
+        <Text>MOdal hai ta shaty hary</Text>
+      </Modal>
+    </ScrollView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Home;
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,8 @@ import "../global.css";
 import { Slot } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+// import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
@@ -9,6 +11,7 @@ export default function layout() {
   return (
     <SafeAreaProvider>
       <SafeAreaView className="flex-1">
+        {/* <StatusBar style="light"/> */}
         <Stack screenOptions={{headerShown:false}}></Stack>
       </SafeAreaView>
     </SafeAreaProvider>
