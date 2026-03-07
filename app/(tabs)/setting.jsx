fb23b5ca3e21c19e1d4083a424d9d06c372f79cd
david-rai <davidkhaling123@gmail.com>
2026-03-07 22:58:16 +0545

    Feat:Better animation modal

--- app/(tabs)/setting.jsx
+++ app/(tabs)/setting.jsx
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 
 export default function setting() {
   return (
     <View>
-      <Text>setting</Text>
+
+      {/* Theme selection */}
+      <View>
+        <Text>Select Theme</Text>
+        <TouchableOpacity>
+          <Text>Dark</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Light</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Automatic</Text>
+        </TouchableOpacity>
+      </View>
+
+
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
