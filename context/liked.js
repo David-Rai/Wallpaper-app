aaa76adfbf619940bc9fbe33a467b8e8525c96a2
david-rai <davidkhaling123@gmail.com>
2026-03-10 23:41:16 +0545

    Feat:Presistance storege using reactnative-asyncstorage

--- context/liked.js
+++ context/liked.js
@@ -1,13 +1,26 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const useLikedStore = create((set) => ({
-  liked: [],
-  addLiked: (link) =>
-    set((state) => {
-      const exist = state.liked.some((l) => l === link);
-      if (exist) {
-        return { liked: state.liked.filter((l) => l !== link) };
-      }
-      return { liked: [...state.liked, link] };
+export const useLikedStore = create(
+  persist(
+    (set, get) => ({
+      liked: [],
+
+      addLiked: (link) => {
+        const exist = get().liked.includes(link);
+        if (exist) {
+          // remove link if it already exists
+          set({ liked: get().liked.filter((l) => l !== link) });
+        } else {
+          // add new link
+          set({ liked: [...get().liked, link] });
+        }
+      },
     }),
-}));
+    {
+      name: "liked-storage", // key in AsyncStorage
+      storage: createJSONStorage(() => AsyncStorage), // where to save
+    },
+  ),
+);
